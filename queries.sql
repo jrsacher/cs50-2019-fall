5b0f1c12e878f72a5efa52fbcfff2311da2b0c5d
Joshua Sacher <31259475+jrsacher@users.noreply.github.com>
2019-10-28 19:54:40 -0400

    Questions for section

---
+++ queries.sql
@@ -0,0 +1,23 @@
+-- How many albums are in the database? Tracks?
+
+
+-- Find all artists that start with "J"
+
+
+-- Find the title of every Blues track
+
+
+-- Add yourself (or someone else) as a new customer
+
+
+-- Change the new entry's phone number to 617-555-1212 (or the like)
+
+
+-- What was the total for the largest invoice?
+
+
+-- Which customer placed that order and what country are they from?
+
+
+-- What are the names of the tracks they purchased?
+
